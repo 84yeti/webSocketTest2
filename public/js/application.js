2730b447ff322f5e27e7d2a01ddaf215239ebacb
kyohei <larcom30@gmail.com>
2014-03-12 02:10:44 +0900

    以下の機能を追加 ・チャット人数 ・過去ログ参照 ・エンターで送信

--- public/js/application.js
+++ public/js/application.js
@@ -1,8 +1,10 @@
-function appendMessage (name, text) {
-  $("#chat").append(name + ":" + text + "<br/>");
+function appendCount (count) {
+  $("#count").text(count);
 }
-
-$("#send").click(function(event) {
+function appendMessage (name, text) {
+  $("#chat").append('<span class="label label-default">' + name + "</span>" + text + "<br/>");
+}
+function sendMessage() {
   var name = $('#username').val();
   var text = $('#msgbody').val();
   if(name && text) {
@@ -10,6 +12,16 @@ $("#send").click(function(event) {
     ws.send(data);
     $('#msgbody').val('');    
   }
+}
+
+$('#msgbody').keypress(function (e) {
+    if ((e.which && e.which == 13) || (e.keyCode && e.keyCode == 13)) {
+        sendMessage();
+    }
+});
+
+$("#send").click(function(event) {
+  sendMessage();
 });
 
 var ws = new WebSocket(location.origin.replace(/^http/, 'ws'));
@@ -18,5 +30,7 @@ ws.onmessage = function(msg) {
   var data = JSON.parse(msg.data);
   if (data.body) {
     appendMessage(data.name, data.body);
+  } else if (data.count) {
+    appendCount(data.count);
   }
 }
--- websocketTest2/backend.rb
+++ websocketTest2/backend.rb
@@ -4,10 +4,11 @@ require 'json'
 module Websockettest2
   class Backend
     KEEPALIVE_TIME = 15
+    MAX_LOG_SIZE = 50
     def initialize(app)
       @app = app
       @clients = []
-      @messages = []
+      @logs = Array.new
     end
 
     def call(env)
@@ -21,11 +22,20 @@ module Websockettest2
           @clients.each do |client|
             client.send({ count: @clients.size }.to_json)
           end
+          @logs.each do |log|
+            p [:logs, log]
+            ws.send(log)
+          end
+
         end
 
         ws.on :message do |event|
           p [:message, event.data]
-          @clients.each { |client| client.send event.data }
+          @clients.each { |client| 
+            client.send event.data
+          }
+          @logs.push event.data
+          @logs.shift if @logs.size > MAX_LOG_SIZE
         end
 
         ws.on :close do |event|
